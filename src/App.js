8fe4b2a69b6b34e0ee369856740e880280a957e5
Praveen <dpraveen453@gmail.com>
2022-09-12 16:51:12 +0530

    commited changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-import ForgotPassword from "./pages/forgetpassword";
-import NewPassword from "./pages/newpassword";
-import LoginPage from "./pages/login";
-import Change_Password from "./components/templates/Auth/ChangePassword/changepassword.js";
-import ExportPage from "./pages/home/export";
-import Home from "./pages/home/home";
+import Section1 from "./components/templates/Section1/Section1";
+import Navbar from "./components/UiCore/Navbar/Navbar";
+
 // import Data_table from "./components/templates/Data_table/data_table";
 // import All_reg from "./components/templates/All_register/Alreg.js";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/forgotpassword" element={<ForgotPassword />} />
-      <Route path="/newpassword" element={<NewPassword />} />
-      <Route path="/changepassword" element={<Change_Password />} />
-      <Route path="/home/export" element={<ExportPage />} />
-      <Route path="/home" element={<Home />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="/" element={<LoginPage />} />
+      
+    // </Routes>
+    <div>
+      <Navbar/>
+      <Section1/>
+      {/* <p>hiiii</p> */}
+    </div>
   );
 }
 
---
+++ src/components/templates/Section1/Section1.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Section1.css"
+
+function Section1(props) {
+    return (
+        <div className='s1-outer-container'>
+            <div className='s1-inner-container1'>
+                <div className='s1-tabbar'>
+                    <p>Home</p>
+                </div>
+                <div>
+
+                </div>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default Section1;
