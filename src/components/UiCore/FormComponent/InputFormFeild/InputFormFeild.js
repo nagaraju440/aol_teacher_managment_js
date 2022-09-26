395dc5a97cf5d686cd57d7f20b4ffda397aff739
bala nagaraju <payyavula@payatu.io>
2022-09-26 23:29:24 +0530

    worked on form

--- src/components/UiCore/FormComponent/InputFormFeild/InputFormFeild.js
+++ src/components/UiCore/FormComponent/InputFormFeild/InputFormFeild.js
@@ -14,6 +14,7 @@ import styles from './InputFormFeild.module.css'
         <div>
       <FormControl  error={!!error?.message} >
       <InputBase
+      
     {...props}
       {...field}
       style={{borderColor:error?.message?'#d32f2f':'black'}}
--- src/components/templates/Auth/NewPassword/NewPassword.js
+++ src/components/templates/Auth/NewPassword/NewPassword.js
@@ -8,6 +8,9 @@ import { InputAdornment, IconButton } from "@mui/material";
 import Form from "../../../UiCore/FormComponent/FormFeild/FormFeild";
 import InputFormFeild from "../../../UiCore/FormComponent/InputFormFeild/InputFormFeild";
 import UiButton from "../../../UiCore/FormComponent/UiButton/UiButton";
+import { useForm, FormProvider, useFormContext, UseFormReturn,useController } from "react-hook-form";
+import { TextField,Input,InputBase,FormHelperText,FormControl } from "@mui/material";
+import { yupResolver } from "@hookform/resolvers/yup";
 const newPasswordSchema = yup.object().shape({
   password: yup
     .string()
@@ -25,22 +28,37 @@ const newPasswordSchema = yup.object().shape({
 export default function NewPassword() {
   const [showPassword,setShowPassword]=useState(true);
   const [showConfirmPassword,setShowConfirmPassword]=useState(true);
-
+   /**
+   * stored the password and confirm password value intwo state variables at here
+   */
+  const [password,setPassword]=useState("")
+  const [confirmpassword,setConfirmPassword]=useState("")
   const onSubmit = (data) => {
     console.log("data is", data);
   };
+
+  const methods = useForm({
+    resolver: yupResolver(newPasswordSchema),
+    mode:"all",
+  });
+  const { handleSubmit,register,formState:{errors}} = methods;
   return (
     <div className={styles.outerContainer}>
       <div className={styles.container}>
         <img src={AOLlogo} className={styles.img} />
         <div className={styles.heading}>New Password</div>
-        <Form onSubmit={onSubmit} schema={newPasswordSchema}>
+        <FormProvider   {...methods} >
+          <form  onSubmit={handleSubmit(onSubmit)} className={styles.form_container}   >
           <label className={styles.label}>Enter New Password</label>
-          <InputFormFeild
+          <FormControl  error={!!errors["password"]?.message} >
+          <InputBase
             label="Enter new password"
             name="password"
             type={showPassword?"text":"password"}
             className={styles.input}
+            {...register("password",{
+              onChange:(e)=>{setPassword(e.target.value)}
+            })}
             variant={"outlined"}
             endAdornment={(
               <InputAdornment position="end">
@@ -48,15 +66,20 @@ export default function NewPassword() {
               {showPassword?<VisibilityOff/>:<Visibility/>}
             </IconButton>
             </InputAdornment>
-            )}
-            
+            )} 
           />
+            <FormHelperText id="my-helper-text">{errors["password"]?.message}</FormHelperText>
+      </FormControl>
+          <div>
           <label className={styles.label}>Confirm Password</label>
-          <InputFormFeild
+          <FormControl  error={!!errors["password"]?.message} >
+          <InputBase
             label="Confirm Password"
             name="confirmpassword"
+              {...register("confirmpassword",{
+                onChange:(e)=>{setConfirmPassword(e.target.value)}
+              })}
             type={showConfirmPassword?"text":"password"}
-            
             className={styles.input}
             variant={"outlined"}
             endAdornment={(
@@ -67,11 +90,15 @@ export default function NewPassword() {
             </InputAdornment>
             )}
           />
+          <FormHelperText id="my-helper-text">{errors["password"]?.message}</FormHelperText>
+      </FormControl>
+          </div>
          <div className={styles.btnContainer} >
          <UiButton text="Save" type="submit"  className={styles.button} ></UiButton>
          </div>
           <div className={styles.link}><Link to="/login" >cancel</Link></div>
-        </Form>
+        </form>
+        </FormProvider>
       </div>
     </div>
   );
