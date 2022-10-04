f6955e367863b88ddd9b97582ffd1a0ca876e099
ManuGM-dev <63191789+ManuGM-dev@users.noreply.github.com>
2022-10-04 17:23:55 +0530

    made changes in stylings

--- src/components/templates/Home/HomePage/HomePage.js
+++ src/components/templates/Home/HomePage/HomePage.js
@@ -1,14 +1,14 @@
-import React, {  } from "react";
+import React from "react";
 import UiButton from "../../../UiCore/FormComponent/UiButton/UiButton";
 import "./HomePage.css";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Data_table from "../../Data_table/data_table";
-import {GridToolbarQuickFilter} from '@mui/x-data-grid'
+import { GridToolbarQuickFilter } from "@mui/x-data-grid";
 import { getDataByRegion } from "../../../../assets/apiCalls/ApiHomePage";
-import { useQuery } from '@tanstack/react-query'
-import {useQueryClient} from '@tanstack/react-query'
+import { useQuery } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
 const regions = [
   "All Regions",
   "Africa",
@@ -23,7 +23,7 @@ const regions = [
   "South America",
   "South Asia",
 ];
-const addTotalTeachersFeild=(data)=>{
+const addTotalTeachersFeild = (data) => {
   var temp = data.map((d) => {
     return {
       ...d,
@@ -34,40 +34,50 @@ const addTotalTeachersFeild=(data)=>{
     };
   });
   return temp;
- }
+};
 function HomePage() {
   const [selectedRegion, setSelectedRegion] = React.useState(0);
   const queryClient = useQueryClient();
   /**
    * fetching data table data using react query.
    */
-  const {data:regionData}=useQuery(
-    ["region-data",regions[selectedRegion]],getDataByRegion,
-    {select:(data)=>{return addTotalTeachersFeild(data.data)}}
-    );
-  const handleChangeSelect=(e)=>{
-    setSelectedRegion(e.target.value)
-    queryClient.invalidateQueries(['region-data'])
-  }
+  const { data: regionData } = useQuery(
+    ["region-data", regions[selectedRegion]],
+    getDataByRegion,
+    {
+      select: (data) => {
+        return addTotalTeachersFeild(data.data);
+      },
+    }
+  );
+  const handleChangeSelect = (e) => {
+    setSelectedRegion(e.target.value);
+    queryClient.invalidateQueries(["region-data"]);
+  };
   /**
    * @returns it returns a toolbar for home page data table.
    */
- const HomePageToolBar=()=>{
-  return(
-    <HomePageToolBarComponent  selectedRegion={selectedRegion} handleChangeSelect={handleChangeSelect} />
-  )
- }
-  return(
+  const HomePageToolBar = () => {
+    return (
+      <HomePageToolBarComponent
+        selectedRegion={selectedRegion}
+        handleChangeSelect={handleChangeSelect}
+      />
+    );
+  };
+  return (
     <div>
       <div className="home-inner-container1">
         <div className="home-inner-container2">
-          {
-            regionData?(<Data_table
+          {regionData ? (
+            <Data_table
               data={regionData}
               height={500}
               toolBar={HomePageToolBar}
-            />):<div>Loading...</div>
-          }
+            />
+          ) : (
+            <div>Loading...</div>
+          )}
         </div>
       </div>
       {/* <div className="summary-container">
@@ -84,61 +94,65 @@ function HomePage() {
 }
 export default HomePage;
 
-
-
-const HomePageToolBarComponent=({selectedRegion,handleChangeSelect})=>{
-  const navigate=useNavigate();
-  return(
+const HomePageToolBarComponent = ({ selectedRegion, handleChangeSelect }) => {
+  const navigate = useNavigate();
+  return (
     <div>
-<div>
-            <div className="home-icon">
-              <div className="home-icon-left-container">
-                <div>
-                  <h3>Home</h3>
-                </div>
-                <div className="slect-dropdown-container">
-                  <Select
-                    sx={{ height: 40 }}
-                    className="select-dropdown"
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    name="region"
-                    value={selectedRegion}
-                    onChange={handleChangeSelect}
-                    defaultValue={10}
-                  >
-                    {regions.map((region, index) => (
-                      <MenuItem value={index} key={index} >{region}</MenuItem>
-                    ))}
-                  </Select>
-                  <GridToolbarQuickFilter  className="home-search" placeholder="Search by Country" variant="outlined" />
-                </div>
-              </div>
-              <div className="home-export-add-container">
-                <UiButton
-                  text="Export"
-                  onClick={() => {
-                    navigate("/home/export", {
-                      state: {
-                        /**
-                         * here we have  to send one more data whihch is region data and we need to change all regions with selectbox value , check Home page copy for refrence
-                         */
-                        selectedRegion: "All Regions",
-                      },
-                    });
-                  }}
-                ></UiButton>
-                <div>
-                  <UiButton
-                    onClick={() => navigate("/teachers/new")}
-                    text={"Add Teacher"}
-                  ></UiButton>
-                </div>
-              </div>
+      <div>
+        <div className="home-icon">
+          <div className="home-icon-left-container">
+            <div>
+              <h3>Home</h3>
             </div>
-          <hr className="hr-line"></hr>
-
+            <div className="slect-dropdown-container">
+              <Select
+                sx={{ height: 40 }}
+                className="select-dropdown"
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                name="region"
+                value={selectedRegion}
+                onChange={handleChangeSelect}
+                defaultValue={10}
+              >
+                {regions.map((region, index) => (
+                  <MenuItem value={index} key={index}>
+                    {region}
+                  </MenuItem>
+                ))}
+              </Select>
+              <GridToolbarQuickFilter
+                sx={{ height: 40 }}
+                className="home-search"
+                placeholder="Search by Country"
+                variant="outlined"
+              />
+            </div>
           </div>
+          <div className="home-export-add-container">
+            <UiButton
+              text="Export"
+              onClick={() => {
+                navigate("/home/export", {
+                  state: {
+                    /**
+                     * here we have  to send one more data whihch is region data and we need to change all regions with selectbox value , check Home page copy for refrence
+                     */
+                    selectedRegion: "All Regions",
+                  },
+                });
+              }}
+            ></UiButton>
+            <div>
+              <UiButton
+                onClick={() => navigate("/teachers/new")}
+                text={"Add Teacher"}
+              ></UiButton>
+            </div>
+          </div>
+        </div>
+        <hr className="hr-line"></hr>
       </div>
-  )
-}
+    </div>
+  );
+};
