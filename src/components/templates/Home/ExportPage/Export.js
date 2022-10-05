04eeab149569d97eb765a2acab578aaaf1997e43
ManuGM-dev <63191789+ManuGM-dev@users.noreply.github.com>
2022-10-05 12:06:10 +0530

    made changes

--- src/components/templates/Home/ExportPage/Export.js
+++ src/components/templates/Home/ExportPage/Export.js
@@ -21,14 +21,18 @@ const addTotalTeachersFeild = (data) => {
   });
   return temp;
 };
-const getSelectedRowsData=(regionData,selectedRows)=>{
-    return regionData.filter((data)=>selectedRows.includes(data.Country));
-}
+const getSelectedRowsData = (regionData, selectedRows) => {
+  return regionData.filter((data) => selectedRows.includes(data.Country));
+};
 
 function Export() {
   const location = useLocation();
-  const [selectedRows, setSelectedRows] = useState(location.state.selectedRows);
-  const [selectedRegion, setSelectedRegion] = useState(location.state.selectedRegion);
+  const [selectedRows, setSelectedRows] = useState(
+    location.state.selectedRows || []
+  );
+  const [selectedRegion, setSelectedRegion] = useState(
+    location.state.selectedRegion
+  );
   const { data: regionData } = useQuery(
     ["region-data", selectedRegion],
     getDataByRegion,
@@ -44,41 +48,41 @@ function Export() {
     XLSX.utils.book_append_sheet(wb, ws, "Teachers Data");
     XLSX.writeFile(wb, "AOL Teachers Information.xlsx");
   };
-  const handleExportSelectedRows = ()=>{
+  const handleExportSelectedRows = () => {
     var wb = XLSX.utils.book_new();
-    var ws = XLSX.utils.json_to_sheet(getSelectedRowsData(regionData,selectedRows));
+    var ws = XLSX.utils.json_to_sheet(
+      getSelectedRowsData(regionData, selectedRows)
+    );
     XLSX.utils.book_append_sheet(wb, ws, "Teachers Data");
     XLSX.writeFile(wb, "AOL Teachers Information.xlsx");
   };
 
- 
-  const ExportPageToolBar=()=>{
+  const ExportPageToolBar = () => {
     return (
-      <ExportPageToolBarComponent  
-      selectedRows={selectedRows}
-      selectedRegion={selectedRegion}
-      handleExportSelectedRows={handleExportSelectedRows}
-      handleExportAllRows={handleExportAllRows}
+      <ExportPageToolBarComponent
+        selectedRows={selectedRows}
+        selectedRegion={selectedRegion}
+        handleExportSelectedRows={handleExportSelectedRows}
+        handleExportAllRows={handleExportAllRows}
       />
-    )
-  }
+    );
+  };
 
   return (
     <div className="export-outer-container">
       <div className="export-inner-container1">
         <div className="export-inner-container2">
-          {
-            regionData?(
-              <Data_table
-            data={regionData}
-            selectedRows={selectedRows}
-            setSelectedRows={setSelectedRows}
-            height={500}
-            toolBar={ExportPageToolBar}
-          />
-            ):"Loading..."
-          }
-
+          {regionData ? (
+            <Data_table
+              data={regionData}
+              selectedRows={selectedRows}
+              setSelectedRows={setSelectedRows}
+              height={500}
+              toolBar={ExportPageToolBar}
+            />
+          ) : (
+            "Loading..."
+          )}
         </div>
       </div>
     </div>
@@ -87,47 +91,50 @@ function Export() {
 
 export default Export;
 
-
-
-const ExportPageToolBarComponent=({selectedRegion,handleExportSelectedRows,handleExportAllRows,selectedRows})=>{
-  const navigate=useNavigate();
+const ExportPageToolBarComponent = ({
+  selectedRegion,
+  handleExportSelectedRows,
+  handleExportAllRows,
+  selectedRows,
+}) => {
+  const navigate = useNavigate();
   function handleClose() {
     navigate(-1);
   }
-   return(
+  return (
     <div>
-            <div className="export-heading-container">
-              <div className="export-heading">Region: {selectedRegion}</div>
-              <div className="export-close">
-                <div className="export-selection-number">
-                  {selectedRows.length} records selected
-                </div>
-                <IconButton>
-                  <CloseIcon fontSize="large" onClick={handleClose} />
-                </IconButton>
-              </div>
-            </div>
-            <div className="export-subheading-container">
-              <div className="export-icon-left-container">
-              <GridToolbarQuickFilter
-                className="home-search"
-                placeholder="Search by Country"
-                variant="outlined"
-              />
-              </div>
-              <div className="export-buttons-container">
-                <UiButton
-                  text="Export Selected Rows"
-                  disabled={selectedRows.length != 0 ? false : true}
-                  onClick={handleExportSelectedRows}
-                ></UiButton>
-                <UiButton
-                  text="Export All Rows"
-                  onClick={handleExportAllRows}
-                ></UiButton>
-              </div>
-            </div>
-          <hr className="hr-line"></hr>
+      <div className="export-heading-container">
+        <div className="export-heading">Region: {selectedRegion}</div>
+        <div className="export-close">
+          <div className="export-selection-number">
+            {selectedRows.length} records selected
           </div>
-   )
-}
+          <IconButton>
+            <CloseIcon fontSize="large" onClick={handleClose} />
+          </IconButton>
+        </div>
+      </div>
+      <div className="export-subheading-container">
+        <div className="export-icon-left-container">
+          <GridToolbarQuickFilter
+            className="home-search"
+            placeholder="Search by Country"
+            variant="outlined"
+          />
+        </div>
+        <div className="export-buttons-container">
+          <UiButton
+            text="Export Selected Rows"
+            disabled={selectedRows.length != 0 ? false : true}
+            onClick={handleExportSelectedRows}
+          ></UiButton>
+          <UiButton
+            text="Export All Rows"
+            onClick={handleExportAllRows}
+          ></UiButton>
+        </div>
+      </div>
+      <hr className="hr-line"></hr>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-const queryClient = new QueryClient()
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <QueryClientProvider client={queryClient}>
-  <BrowserRouter>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
-  <ReactQueryDevtools initialIsOpen={false} />
-</QueryClientProvider>
+    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+  </QueryClientProvider>
   // </React.StrictMode>
 );
 
